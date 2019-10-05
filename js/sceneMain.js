9d71fa4e5ae57a8318d44e47e7eb9eec44855fad
katestarks <katestarks@me.com>
2019-10-05 13:58:38 +0100

    lightswitch fades up and down

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -13,6 +13,15 @@ class SceneMain extends Phaser.Scene {
         this.centerX = game.config.width/2;
         this.centerY = game.config.height/2;
 
+        // placing light 'switch' on screen
+        this.lightswitch = this.physics.add.sprite(50, 0, 'star');
+
+
+        //CREATE ALL ASSETS ABOVE THIS LINE
+
+        // Darkness rectangle
+
+
         // placing hero in the center of the screen
         this.hero = this.physics.add.sprite(this.centerX, this.centerY, 'hero');
 
@@ -21,9 +30,6 @@ class SceneMain extends Phaser.Scene {
 
         // generate keyboard keys
         this.cursors = this.input.keyboard.createCursorKeys();
-
-        // placing light 'switch' on screen
-        this.lightswitch = this.physics.add.sprite(50, 0, 'star');
 
         // collider between lightswitch and edge of the scene
         this.lightswitch.body.collideWorldBounds = true;
@@ -46,29 +52,7 @@ class SceneMain extends Phaser.Scene {
             this.hero.setVelocityY(0);
         }
         
-        console.log(this.distanceFromHero(this.lightswitch))
-        this.fadeIn(this.lightswitch);
-        this.fadeOut(this.lightswitch);
-    }
-
-    fadeIn(el)
-    {
-        el.alpha += 0.1;
-        if(el.alpha > 1.0) {
-            el.alpha = 1.0;
-        } else {
-            setTimeout(() => this.fadeIn(el), 500);
-        }
-    }
-
-    fadeOut(el)
-    {
-        el.alpha -= 0.1;
-        if(el.alpha < 0.0) {
-            el.alpha = 0.0;
-        } else {
-            setTimeout(() => this.fadeOut(el), 500);
-        }
+        this.setLightToAlpha(this.distanceFromHero(this.lightswitch), 250)
     }
 
     distanceFromHero(el) 
@@ -77,7 +61,14 @@ class SceneMain extends Phaser.Scene {
         let yCoord = Math.abs(el.body.y - this.hero.body.y)
 
         return xCoord + yCoord
+    }
 
+    setLightToAlpha(distance, scale) {
+        let alpha = 1 - (distance/scale)
+        if (alpha < 0) {
+            alpha = 0
+        }
+        this.lightswitch.alpha = alpha
     }
 
 }
