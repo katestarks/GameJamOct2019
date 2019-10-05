0d372df5dee079e1c99bbbb2d0e9a7de7dc3fd9e
Rauwomos <rowan.walshe@btopenworld.com>
2019-10-05 16:57:58 +0100

    Attempt at turning on/off light with timer, after switch is stepped on

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -6,24 +6,45 @@ class SceneMain extends Phaser.Scene {
 
         this.load.image('hero', 'images/hero.png');
         this.load.image('star', 'images/star.png')
+        this.load.image('background', 'images/background.png')
+        this.load.image('foreground', 'images/black_layer.png')
+        this.load.image('mask', 'images/light-mask.png')
     }
     create() {
+
+        console.log(this)
+
+        var background = this.add.image(0, 0, 'background')
+        background.scaleX = this.game.config.width / background.scaleX 
+        background.scaleY = this.game.config.height / background.scaleY 
 
         // vars to set obj in the center of the game screen
         this.centerX = this.game.config.width/2;
         this.centerY = this.game.config.height/2;
 
-        // placing light 'switch' on screen
-        this.lightswitch = this.physics.add.sprite(50, 50, 'star');
-
+        // placing hero in the center of the screen
+        this.hero = this.physics.add.sprite(this.centerX, this.centerY, 'hero');
 
         //CREATE ALL ASSETS ABOVE THIS LINE
 
         // Darkness rectangle
+        this.foreground = this.add.image(0, 0, 'foreground')
+        this.foreground.scaleX = this.game.config.width / this.foreground.scaleX
+        this.foreground.scaleY = this.game.config.height / this.foreground.scaleY
+        
+        this.spotlight = this.make.sprite({
+            x: 300,
+            y: 300,
+            key: 'mask',
+            add: false
+        });
+        this.spotlight.alpha = 0
 
+        this.foreground.mask = new Phaser.Display.Masks.BitmapMask(this, this.spotlight)
+        this.foreground.mask.invertAlpha = true
+        this.foreground.mask.bitmapMask.scale = 3
+        console.log(this.foreground.mask)
 
-        // placing hero in the center of the screen
-        this.hero = this.physics.add.sprite(this.centerX, this.centerY, 'hero');
 
         // collider between hero and edge of the scene
         this.hero.body.collideWorldBounds = true;
@@ -31,10 +52,15 @@ class SceneMain extends Phaser.Scene {
         // generate keyboard keys
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        // placing light 'switch' on screen
+        this.lightswitch = this.physics.add.sprite(50, 50, 'star');
+
         // Lightswitch scale and initial alpha
         this.lightswitch.setScale(2);
         this.setLightToAlpha(this.distanceFromHero(this.lightswitch), 250)
 
+        this.physics.add.overlap(this.hero, this.lightswitch, this.turnOnLight, null, this);
+        this.pressedLightSwitch = false
     }
 
     update() {
@@ -56,8 +82,6 @@ class SceneMain extends Phaser.Scene {
 
         // If moving diagonally, limit the speed to the same as if you were moving along only one axis
         if(this.hero.body.velocity.x && this.hero.body.velocity.y) {
-            
-
             this.hero.body.velocity.x *= Math.SQRT1_2
             this.hero.body.velocity.y *= Math.SQRT1_2
         }
@@ -65,6 +89,8 @@ class SceneMain extends Phaser.Scene {
         // If hero is moving in any direction
         if (this.hero.body.velocity.x || this.hero.body.velocity.y) {
             this.setLightToAlpha(this.distanceFromHero(this.lightswitch), 250)
+            this.foreground.mask.bitmapMask.x = this.hero.x
+            this.foreground.mask.bitmapMask.y = this.hero.y
         }
     }
 
@@ -84,4 +110,37 @@ class SceneMain extends Phaser.Scene {
         this.lightswitch.alpha = alpha
     }
 
+    turnOnLight() {
+        if (!this.pressedLightSwitch) {
+            this.tweens.add({
+                targets: this.spotlight,
+                alpha: 1,
+                duration: 2000,
+                ease: 'Sine.easeIn',
+                onComplete: () => {this.tweens.add({
+                    targets: this.spotlight,
+                    alpha: 0,
+                    duration: 10000,
+                    ease: 'Sine.easeIn',
+                })}
+            });
+            this.pressedLightSwitch = true
+        }
+        // The above sort of works, but I would rather the spotlight get bigger, than brighter
+        // if (!this.pressedLightSwitch) {
+        //     this.tweens.add({
+        //         targets: this.foreground.mask.bitmapMask,
+        //         scale: 1,
+        //         duration: 2000,
+        //         ease: 'Sine.easeIn',
+        //         onComplete: () => {this.tweens.add({
+        //             targets: this.foreground.mask.bitmapMask,
+        //             scale: 0,
+        //             duration: 10000,
+        //             ease: 'Sine.easeIn',
+        //         })}
+        //     });
+        //     this.pressedLightSwitch = true
+        // }
+    }
 }
