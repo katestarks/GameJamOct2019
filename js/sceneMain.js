a1d9f71e062ef36676c246fb097d1622a1157023
Alessandro Corra <alecorra1980@gmail.com>
2019-10-05 10:29:37 +0100

    first commit, game template

---
+++ js/sceneMain.js
@@ -0,0 +1,18 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload() {
+        // load our images or sounds
+    }
+    create() {
+        console.log("Ready!");
+        //  define our objects
+    }
+    update() {
+        //  constant running loop
+    }
+    customFunctions() {
+        //  our custom functions to call them when we need
+    }
+}
