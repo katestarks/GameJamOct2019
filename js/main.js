eae32fc2b98668e7e52e6de94e9d0efb5921a045
trichoplax <githubphagocyte@gmail.com>
2019-10-05 14:16:52 +0100

    Use 16:9 aspect ratio

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ window.onload = function () {
 
     var config = {
         type: Phaser.AUTO,
-        width: 800,
-        height: 600,
+        width: 1024,
+        height: 576,
         parent: 'phaser-game',
         scene: [SceneMain]
     };
