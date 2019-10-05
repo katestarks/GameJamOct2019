6cb9aa623de486513a860c7ee83e696f6415936c
katestarks <katestarks@me.com>
2019-10-05 14:26:11 +0100

    changed screen size

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@ window.onload = function () {
 
     var config = {
         type: Phaser.AUTO,
-        width: 480,
-        height: 640,
+        width: 600,
+        height: 600,
         physics: {
-        default: 'arcade',
-        arcade: {
-        debug: false
-        }
-    },
+            default: 'arcade',
+            arcade: {
+                debug: false
+            }
+        },
         parent: 'phaser-game',
         physics: {
-        default: 'arcade',
-        arcade: {
-        debug: true
-        }
-    },
+            default: 'arcade',
+            arcade: {
+                debug: true
+            }
+        },
         scene: [SceneMain]
     }
 
